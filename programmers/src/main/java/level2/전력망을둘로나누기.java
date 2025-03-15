71d171f5014e54b2527abfc8118cc539c71795fc
Seongjae Hong <sjhong310@gmail.com>
2025-03-15 21:46:32 +0900

    전력망 둘로 나누기

--- programmers/src/main/java/level2/전력망을둘로나누기.java
+++ programmers/src/main/java/level2/전력망을둘로나누기.java
@@ -1,146 +1,72 @@
 package level2;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/86971
 public class 전력망을둘로나누기 {
-    static class Node {
-        int data;
-        Node parent;
-        LinkedList<Node> children = new LinkedList<>();
-        int cntDescendant;
-
-        Node(int data) {
-            this.data = data;
-        }
-
-        boolean equals(int data) {
-            return this.data != data;
-        }
-    }
+    ArrayList<ArrayList<Integer>> nodes;
 
     public int solution(int n, int[][] wires) {
-        Node tree = makeTree(n, wires);
-        int diff = tree.cntDescendant - tree.children.getFirst().cntDescendant * 2 - 1;
-        if (diff < 0) {
-            diff *= -1;
+        nodes = new ArrayList<>(n + 1);
+
+        for (int i = 1; i <= n + 1; i++) {
+            nodes.add(new ArrayList<>());
         }
-        int answer = getDiff(diff, tree.children.getFirst(), tree);
 
-        return answer;
-    }
+        for (int[] wire : wires) {
+            nodes.get(wire[0]).add(wire[1]);
+            nodes.get(wire[1]).add(wire[0]);
+        }
 
-    int getDiff(int minDiff, Node tree, Node root) {
-        System.out.println("minDiff = " + minDiff);
+        int minDiff = n;
+        int cnt;
         int diff;
-        for (Node node: tree.children) {
-            diff = root.cntDescendant - node.cntDescendant * 2 - 1;
-            if (diff == 0) {
-                return 0;
-            }
+        boolean[] visits;
+        for (int[] wire : wires) {
+            nodes.get(wire[0]).remove(Integer.valueOf(wire[1]));
+            nodes.get(wire[1]).remove(Integer.valueOf(wire[0]));
 
+            visits = new boolean[n + 1];
+            visits[1] = true;
+            cnt = size(visits, 1);
+            diff = n - 2 * cnt;
             if (diff < 0) {
                 diff *= -1;
             }
-            System.out.println("curDiff: " + diff + " (" + "Node: " + node.data + ")");
-
             if (diff < minDiff) {
                 minDiff = diff;
             }
 
-            diff = getDiff(minDiff, node, root);
-            if (diff < minDiff) {
-                minDiff = diff;
-            }
+            nodes.get(wire[0]).add(wire[1]);
+            nodes.get(wire[1]).add(wire[0]);
         }
 
         return minDiff;
     }
 
-    Node makeTree(int n, int[][] wires) {
-        Node root = new Node(wires[0][0]);
-        Node parent;
-        Node child;
-        child = new Node(wires[0][1]);
-        child.parent = root;
-        root.children.add(child);
-        root.cntDescendant += 1;
+    int size(boolean[] visits, int curNode) {
+        int cnt = 1;
 
-        for (int i=1; i < n; i++) {
-            Node parentNode = findParentNode(root, wires[i]);
-            if (parentNode.equals(wires[i][0])) {
-                child = new Node(wires[i][0]);
-            } else {
-                child = new Node(wires[i][1]);
-            }
-            child.parent = parentNode;
-            parentNode.children.add(child);
-
-            // 부모별 전체 자식 노드 개수 증가
-            parentNode.cntDescendant += 1;
-            parent = parentNode.parent;
-            while (parent != root) {
-                parent.cntDescendant += 1;
-                parent = parent.parent;
-            }
-            root.cntDescendant += 1;
-        }
-
-        return root;
-    }
-
-    Node findParentNode(Node node, int[] data) {
-        if (node.equals(data[0]) && node.equals(data[1])) {
-            if (node.children.isEmpty()) {
-                return null;
-            }
-
-            Node parentNode;
-            for (Node child: node.children) {
-                parentNode = findParentNode(child, data);
-                if (parentNode != null) {
-                    return parentNode;
-                }
+        for (Integer node : nodes.get(curNode)) {
+            if (!visits[node]) {
+                visits[node] = true;
+                cnt += size(visits, node);
             }
         }
-
-        return node;
+        return cnt;
     }
+
 
     public static void main(String[] args) {
-        int[][] arr1 = new int[][]{{1,3},{2,3},{3,4},{4,5},{4,6},{4,7},{7,8},{7,9}};
-        int[][] arr2 = new int[][]{{1,2},{2,7},{3,7},{3,4},{4,5},{6,7}};
-        int[][] arr3 = new int[][]{{1,2},{2,3},{3,4}};
-        int[][] arr4 = new int[][]{{1,2}, {2,3}};
+        int[][] arr1 = new int[][]{{1, 3}, {2, 3}, {3, 4}, {4, 5}, {4, 6}, {4, 7}, {7, 8}, {7, 9}};
+        int[][] arr2 = new int[][]{{1, 2}, {2, 3}, {3, 4}};
+        int[][] arr3 = new int[][]{{1, 2}, {2, 7}, {3, 7}, {3, 4}, {4, 5}, {6, 7}};
+        int[][] arr4 = new int[][]{{1, 2}, {2, 3}};
+        int[][] arr5 = new int[][]{{1, 3}, {2, 3}, {3, 4}, {4, 5}, {4, 6}, {4, 7}, {7, 8}, {7, 9}, {9, 10}, {10, 11}};
         전력망을둘로나누기 cls = new 전력망을둘로나누기();
 
-        int[][] arr = arr1;
-        Node tree = cls.makeTree(arr.length, arr);
-//        printNode(tree);
-        int diff = cls.solution(arr.length, arr);
-        System.out.println("Solution = " + diff);
-    }
-
-    static void printNode(Node tree) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(tree.data).append(": [");
-
-        if (tree.children.isEmpty()) {
-            return;
-        }
-
-        for (int i=0; i<tree.children.size()-1; i++) {
-            sb.append(tree.children.get(i).data);
-            sb.append("(").append(tree.children.get(i).cntDescendant).append(")");
-            sb.append(", ");
-        }
-        sb.append(tree.children.getLast().data);
-        sb.append("(").append(tree.children.getLast().cntDescendant).append(")");
-        sb.append("]");
-        System.out.println(sb);
-
-        for (Node child: tree.children) {
-            printNode(child);
-        }
+        int[][] arr = arr5;
+        int diff = cls.solution(arr.length + 1, arr);
+        System.out.println("diff = " + diff);
     }
 }
